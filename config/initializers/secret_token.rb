15cdbb707d21dc335ff84d2c652e35db20130002
Arpit1989 <speak2arpit@gmail.com>
2013-06-30 13:41:30 +0530

    First push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '2d888244af6adc7a2550ea2a0fc7351247b847475bbe178d8c1c0b12f74144904f9d852de6996e8e8080626c05a922d264730c2692cff0e5ae87296e635481a6'
